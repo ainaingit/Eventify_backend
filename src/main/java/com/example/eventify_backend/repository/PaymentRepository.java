3a35ddd1bf3b99cfe902fb3dbbe1342b8cdb4b5c
RAMAROSON Aina Kevin <ainakevinramaroson@gmail.com>
2025-03-05 15:22:40 +0300

    Correct payload

--- src/main/java/com/example/eventify_backend/repository/PaymentRepository.java
+++ src/main/java/com/example/eventify_backend/repository/PaymentRepository.java
@@ -8,6 +8,4 @@ import org.springframework.stereotype.Repository;
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
 
     public Payment getPaymentById(Long Id);
-    public Payment getPaymentByPaymentId(Long Id);
-    public Payment getPaymentByUserId(Long Id);
 }
